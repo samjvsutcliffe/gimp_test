a9501c70a6805b422d329d78541569f70beb194b
Sam <sam.sutcliffe2@gmail.com>
2023-01-27 17:49:13 +0000

    Removed clrf

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Running MPM"
-(cd ./test_cases/mpm ;./consol_batch.sh)
+(cd ./test_cases/mpm ;./run.sh)
 echo "Running GIMP"
-(cd ./test_cases/gimp ;./consol_batch.sh)
+(cd ./test_cases/gimp ;./run.sh)
