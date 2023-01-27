bbd0a9360fb1e5072b6a6e39d59e33ca27a440d0
Sam <sam.sutcliffe2@gmail.com>
2023-01-27 17:23:53 +0000

    Update run test

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
-export PATH="$PATH ../mpm/build"
+#!/bin/sh
 echo "Running MPM"
 (cd ./test_cases/mpm/; ./consol_batch.sh)
 echo "Running GIMP"
-(cd ./test_cases/gimp/; ./consol_batch.sh)
+(cd ./test_cases/gimp/; sh consol_batch.sh)
